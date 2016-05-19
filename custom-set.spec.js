e7cce0485ff3e7f4d5873dd44f20c1303e885753
Romeeka Gayhart <rrgayhart@gmail.com>
2016-05-19 06:45:18 -0600

    Update custom-set.spec.js

--- custom-set.spec.js
+++ custom-set.spec.js
@@ -1,7 +1,6 @@
 var CustomSet = require('./custom-set');
 
 describe('CustomSet', function() {
-
   it('can compare equality of sets', function(){
     var subject = new CustomSet([3, 2, 1]);
     var duplicateSubject = new CustomSet([3, 2, 1]);
@@ -14,4 +13,22 @@ describe('CustomSet', function() {
     expect(subject.eql(nonDuplicateSubject)).toBe(false);
   });
   
+  xit('can delete elements', function(){
+    var expected = new CustomSet([1, 3]);
+    var actual = new CustomSet([3, 2, 1]).delete(2);
+    expect(actual.eql(expected)).toBe(true);
+
+    var expected2 = new CustomSet([1, 2, 3]);
+    var actual2 = new CustomSet([3, 2, 1]).delete(4);
+    expect(actual2.eql(expected2)).toBe(true);
+  });
+
+  xit('can check for difference', function(){
+    var expected = new CustomSet([1, 3]);
+    var actual = new CustomSet([3, 2, 1]).difference(new CustomSet([2, 4]));
+    expect(actual.eql(expected)).toBe(true);
+    var expected2 = new CustomSet([1, 2, 3]);
+    var actual2 = new CustomSet([1, 2, 3]).difference(new CustomSet([4]));
+    expect(actual2.eql(expected2)).toBe(true);
+  });
 });
